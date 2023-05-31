535598b5f99a0cf3fc02a0cee0f06f87d5060a30
asrar-codes <asrarmaqbool06@gmail.com>
2023-05-31 14:49:29 +0530

    more on functions

---
+++ 14Functions.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+// An = a + (n - 1) d ~ nth term of A.P
+
+int noOfSetBits(int a, int b)
+{
+
+ int n = 0;
+ while (a != 0)
+ {
+  if (a & 1)
+  {
+   n += 1;
+  }
+
+  a = a >> 1;
+ }
+ while (b != 0)
+ {
+  if (b & 1)
+  {
+   n += 1;
+  }
+  b = b >> 1;
+ }
+
+ cout << n;
+ return n;
+}
+
+int fibonacci()
+{
+ int n;
+ cin >> n;
+ int fib = 0;
+ for (int i = 3; i <= n; i++)
+ {
+  fib = i - 1 + (i - 2);
+  cout << fib << " ";
+ }
+ return fib;
+}
+
+int main()
+{
+
+ /*
+
+ // prints the nth term of AP;
+   int n;
+  cin >> n;
+
+  cout << (3 * n) + 7;
+   */
+
+ // prin the total number of set bits;
+ /*
+   a:2 ==> 10
+   b:3:==> 11
+
+   no of set bits is 3
+   */
+
+ /*  int a, b;
+  cin >> a >> b;
+
+  noOfSetBits(a, b); */
+
+ fibonacci();
+}
